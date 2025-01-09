b6e89ff22db1f7b861ccf4998a1fa638480f9e5c
Victor-vrg <victorvrg2003@gmail.com>
2025-01-09 17:36:10 -0300

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  experimental: {
+    svg: true,
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
