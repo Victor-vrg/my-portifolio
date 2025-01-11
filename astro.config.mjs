73772df21a8f67ea725ad0c198d3c5e16ede7f36
Victor-vrg <victorvrg2003@gmail.com>
2025-01-10 22:40:29 -0300

    adaptador Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-
-import node from "@astrojs/node";
 import image from '@astrojs/image';
+import netlify from "@astrojs/netlify"; // Importar o adaptador do Netlify
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image()],
   experimental: {
     svg: true,
   },
-  
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(), 
 });
