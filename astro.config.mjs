ef68133ed99effdd99ba3a200991a30bd2ed14ed
Victor-vrg <victorvrg2003@gmail.com>
2025-01-10 22:26:02 -0300

    fix: build erros

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
-
+import image from '@astrojs/image';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()],
   experimental: {
     svg: true,
   },
+  
   adapter: node({
     mode: "standalone",
   }),
